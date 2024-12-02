5ee1bdf9fd577b6041d68dd73bd4af0a55255cfd
Muaz Abdin <muathabd25@gmail.com>
2024-12-02 22:51:40 +0200

    create the main entry point of the program

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
+import AssociationDataAnalyzer from "./dataAnalyzers/AssociationDataAnalyzer.js";
 import InstanceDataCollector from "./dataCollectors/InstanceDataCollector.js";
+import ConsoleDisplay from "./resultDisplay/ConsoleDisplay.js";
+import Logger from "./utils/logger.js";
 
 async function run() {
   try {
+    Logger.info("Starting data collection process");
     const instanceDataCollector = new InstanceDataCollector();
     const allInstances = await instanceDataCollector.getAll();
-    console.log(allInstances);
+
+    const associationDataAnalyzer = new AssociationDataAnalyzer();
+    const { compliantInstances, nonCompliantInstances } =
+      await associationDataAnalyzer.checkCompliance(allInstances);
+    const tableData = associationDataAnalyzer.SummrizeResults(nonCompliantInstances);
+
+    ConsoleDisplay.display({
+      title: "NON-COMPLIANT MANAGED INSTANCES",
+      header: ["InstanceId", "Association", "Severity", "Status"],
+      items: tableData,
+    });
+
+    Logger.info("Data collection and analysis complete");
   } catch (error) {
-    console.error("An error occurred:", error);
+    Logger.error("An unexpected error occurred", error);
   }
 }
 
